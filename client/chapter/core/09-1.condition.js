ed60c49bb4ac8fecf3edf9d5e03915041002dcb9
llhyeon <0617mosses@gmail.com>
2024-10-31 16:15:44 +0900

    기본 세팅 수정 및 JS 문 연습

---
+++ client/chapter/core/09-1.condition.js
@@ -0,0 +1,55 @@
+/* ---------------- */
+/* Condition        */
+/* ---------------- */
+
+// const userAnswer = prompt("자바스크립트의 '공식' 이름은 무엇일까요 ?");
+
+// if (userAnswer === 'ECMAScript') {
+//   alert('정답입니다 !');
+// } else {
+//   alert('모르셨나요 ? 정답은 ECMAScript 입니다 !');
+// }
+
+// const userAnswer = prompt('숫자를 입력하세요');
+
+// if (userAnswer > 0) {
+//   console.log(1);
+// } else if (userAnswer < 0) {
+//   console.log(-1);
+// } else {
+//   console.log(0);
+// }
+
+// const result = (a + b < 4) ? "미만" : "이상";
+
+let message =
+  login == '직원'
+    ? '안녕하세요'
+    : login == '임원'
+      ? '환영합니다'
+      : login == ''
+        ? '로그인이 필요합니다.'
+        : '';
+
+// 그 영화 봤니?
+//     ↓
+// Yes | No
+//     | 영화 볼거니?
+//           ↓
+//       Yes | No
+
+// 영화 봤니?
+let didWatchMovie;
+
+// 영화 볼거니?
+let goingToWatchMovie;
+
+// if 문(statement)
+
+// else 절(clause)
+
+// else if 복수 조건 처리
+
+// 조건부 연산자
+
+// 멀티 조건부 연산자 식
