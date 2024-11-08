878ee850bcf2c0590ab6ab19611cb6a6a432a04b
llhyeon <0617mosses@gmail.com>
2024-11-08 09:20:29 +0900

    함수 완료

--- client/chapter/core/11-3.function.js
+++ client/chapter/core/11-3.function.js
@@ -16,17 +16,56 @@ let resultZ = calculateTotal(9000, -2500, 5000, 11900);
 
 // 함수 선언 → 화살표 함수 (표현)식
 let calcAllMoney = (...rest) => {
-  console.log(rest);
+  let total = 0;
+  for (let i = 0; i < rest.length; i++) {
+    total += rest[i];
+  }
+  console.log(total);
+
+  let total2 = 0;
+  for (let i of rest) {
+    total2 += i;
+  }
+  console.log(total2);
+
+  let total3 = 0;
+  rest.forEach((ele) => {
+    total3 += ele;
+  });
+  console.log(total3);
+
+  const total4 = rest.reduce((acc, cur) => acc + cur, 0);
+  console.log(total4);
 };
 
-calcAllMoney(1000, 2000, 3000, 4000);
+// calcAllMoney(1000, 2000, 3000, 4000);
 
 // 화살표 함수와 this
 
 /* 다음 함수를 작성해봅니다. -------------------------------------------------- */
 
 // pow(numeric: number, powerCount: number): number;
-let pow;
+// let pow = (numeric, powerCount) => {
+//   let total = 1;
+//   for (let i = 0; i <= powerCount; i++) {
+//     total *= numeric;
+//   }
 
+//   return total;
+// };
+
+const pow = (numeric, powerCount) =>
+  Array(powerCount)
+    .fill(null)
+    .reduce((acc) => {
+      return (acc *= numeric);
+    }, 1);
+// console.log(pow(2, 53));
 // repeat(text: string, repeatCount: number): string;
-let repeat;
+let repeat = (text, repeatCount) => {
+  return Array(repeatCount)
+    .fill(null)
+    .reduce((acc) => (acc += text), '');
+};
+
+console.log(repeat('배고파', 3));
