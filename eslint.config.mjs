4345f4f47e10d09e30bc58573ad2e2547cc78017
llhyeon <0617mosses@gmail.com>
2024-11-08 09:20:16 +0900

    기본 설정 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+const type = {
+  isObject: true,
+  isArray: true,
+};
 
 export default [
   pluginJs.configs.recommended,
@@ -8,10 +13,11 @@ export default [
       globals: {
         ...globals.browser,
         ...globals.node,
+        ...type,
       },
     },
     rules: {
-      "no-unused-vars": "warn",
+      'no-unused-vars': 'warn',
     },
   },
 ];
