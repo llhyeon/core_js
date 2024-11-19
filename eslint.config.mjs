b7aba10cd7d0062b64fd316408c56f0ada488f7e
llhyeon <0617mosses@gmail.com>
2024-11-19 16:29:43 +0900

    eslint 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,6 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 
-const type = {
-  isObject: true,
-  isArray: true,
-  isString: true,
-  isNumber: true,
-  isNull: true,
-  isUndefined: true,
-  isFunction: true,
-};
-
-const dom = {
-  getNode: true,
-  getAttr: true,
-  setAttr: true,
-  attrs: true,
-  insertBefore: true,
-  insertFirst: true,
-  insertLast: true,
-  insertAfter: true,
-  getCss: true,
-  setCss: true,
-  css: true,
-  addClass: true,
-  removeClass: true,
-  toggleClass: true,
-  clearContents: true,
-};
-
-const error = {
-  defaultError: true,
-  typeError: true,
-  syntaxError: true,
-  refError: true,
-};
-
 export default [
   pluginJs.configs.recommended,
   {
@@ -43,9 +8,6 @@ export default [
       globals: {
         ...globals.browser,
         ...globals.node,
-        ...type,
-        ...dom,
-        ...error,
       },
     },
     rules: {
