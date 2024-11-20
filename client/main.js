a7aa0e2ece18ff08159f160101b5cdfed6381a75
llhyeon <0617mosses@gmail.com>
2024-11-20 11:46:18 +0900

    주접 생성기

--- client/main.js
+++ client/main.js
@@ -1,64 +1,32 @@
-// import { getNode, clearContents } from './lib/dom/index.js';
+import data from './data/data.js';
+import { addClass, getNode as $, clearContents, insertLast, getRandom, removeClass, showAlert, isNumber, isNumericString, shake } from './lib/index.js';
 
-import { getNode as $, clearContents, insertLast } from './lib/index.js';
+const submit = $('#submit');
+const input = $('#nameField');
+const result = $('.result');
 
-function phase1() {
-  const firstInput = getNode('#firstNumber');
-  const secondInput = getNode('#secondNumber');
-  const result = getNode('.result');
-  const clear = getNode('#clear');
+function handleSubmit(e) {
+  e.preventDefault();
+  const name = input.value;
 
-  function handleClear(e) {
-    e.preventDefault();
-    clearContents(firstInput);
-    clearContents(secondInput);
-    clearContents(result);
+  if (!name || name.replaceAll(' ', '') === '') {
+    showAlert('.alert-error', '공백은 허용되지 않습니다.', 1200);
+    shake(input);
+    return;
   }
 
-  function handleInput() {
-    const firstValue = firstInput.value;
-    const secondValue = secondInput.value;
-
-    result.textContent = +firstValue + +secondValue;
+  if (!isNumericString(name)) {
+    showAlert('.alert-error', '정확한 이름을 입력해주세요', 1200);
+    shake(input);
+    return;
   }
+  const list = data(name);
 
-  // function handleCalculator(e) {
-  //   e.preventDefault();
+  const randomIndex = getRandom(list.length);
 
-  //   let firstValue = firstInput.value;
-  //   let secondValue = secondInput.value;
-
-  //   result.textContent = Number(firstValue) + Number(secondValue);
-
-  //   const clear = getNode('#clear');
-  // }
-
-  // form.addEventListener('submit', handleCalculator);
-  firstInput.addEventListener('input', handleInput);
-  secondInput.addEventListener('input', handleInput);
-  clear.addEventListener('click', handleClear);
+  const jujeob = list[randomIndex];
+  clearContents(result);
+  insertLast(result, jujeob);
 }
 
-function phase2() {
-  const calculator = $('.calculator');
-  const result = $('.result');
-  const clear = $('#clear');
-  const numberInputs = [...document.querySelectorAll('input:not(#clear)')];
-
-  function handleInput() {
-    const total = numberInputs.reduce((acc, cur) => acc + +cur.value, 0);
-    clearContents(result);
-    insertLast(result, total);
-  }
-
-  function handleClear(e) {
-    e.preventDefault();
-    numberInputs.forEach(clearContents);
-    clearContents(result);
-  }
-
-  calculator.addEventListener('input', handleInput); // form 태그도 input 이벤트 바인딩 가능
-  clear.addEventListener('click', handleClear);
-}
-
-phase2();
+submit.addEventListener('click', handleSubmit);
