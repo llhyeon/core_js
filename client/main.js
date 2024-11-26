5e2d78b6f36648c75d04e04e658a00f48a5710da
llhyeon <0617mosses@gmail.com>
2024-11-26 14:43:58 +0900

    중간 커밋

--- client/main.js
+++ client/main.js
@@ -1,24 +1,49 @@
-import { deleteStorage, getNode, getStorage, setStorage } from './lib/index.js';
+// console.log(temp.content.cloneNode(true));
 
-const input = getNode('#textField');
-const clearButton = getNode('button[data-name="clear"]');
+class MyElement extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
 
-function handleInput() {
-  setStorage('userInput', input.value);
+  handleClick() {
+    console.log('click !');
+  }
+
+  connectedCallback() {
+    this.render();
+    this.card = this.shadowRoot.querySelector('.card');
+
+    this.card.addEventListener('click', () => {
+      this.handleClick();
+    });
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = /* html */ `
+    <style>
+      .card {
+        background: #eee
+      }
+    </style>
+      <div class="card">
+        <h2>Card Title</h2>
+        <slot name="me"></slot>
+        <p>description..</p>
+      </div>
+    `;
+    console.log(this.shadowRoot);
+  }
+
+  disconnectedCallback() {}
+
+  static get observedAttributes() {
+    return [];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(name, oldValue, newValue);
+  }
 }
 
-function handleClear() {
-  input.value = '';
-  deleteStorage().then(() => {
-    alert('삭제 완료');
-  });
-}
-
-function init() {
-  getStorage('userInput').then((res) => {
-    input.value = res;
-  });
-}
-clearButton.addEventListener('click', handleClear);
-input.addEventListener('input', handleInput);
-init();
+customElements.define('my-element', MyElement);
